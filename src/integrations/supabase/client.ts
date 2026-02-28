53d964f3c888477902813bfcb69c248b32ec7acd
kishoremk05 <kishore.05mk@gmail.com>
2026-03-01 00:30:27 +0530

    new

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -27,5 +27,6 @@ export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABL
     persistSession: true,
     autoRefreshToken: true,
     detectSessionInUrl: true,
+    flowType: 'pkce',
   }
 });
